777eb94f0b56d73805a83a4f4789d70e94cc7ed2
nassiimz <168569609+nassiimz@users.noreply.github.com>
2024-12-18 10:07:51 +0100

    Update station.c

--- station.c
+++ station.c
@@ -3,22 +3,28 @@
 #include "avl.h"
 #include "Station.h"
 
-
-
-void lectureFichier(File*fichier){
-
-    
+void verif_erreur(File * file){
+    if(file == NULL){
+        printf("Il y a une erreur lors de l'ouverture du fichier\n");
+        exit(1);
+    }
 }
+int* recupValFichier(const char *nomFichier){
+    File *fichier;
+    Station * val;
+
+    fichier = fopen(nomFichier, "r");
+    verif_erreur(ficher);
+
+    printf("\n les entier dans le fichier")
+
+}
+
+
 
 
 int somme(AVL* s){
 
-
-
-
   
   
-
-
-
 }
