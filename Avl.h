2ee5be20f6836dec53b4cf73d33710cf52687690
Ilannb <ilannboudria@gmail.com>
2024-12-18 09:57:43 +0100

    Update Avl.h

--- Avl.h
+++ Avl.h
@@ -1,22 +1,23 @@
 #ifndef AVL_H
+#include "Station.h"
 #define AVL_H
 
 typedef struct AVL {
     struct AVL* fg;
     struct AVL* fd;
     int eq;
-    int val;
+    Station val;
 }AVL;
 
-AVL* creerAVL(int r);
+AVL* creerAVL(Station s);
 int estVide(AVL* a);
 int estFeuille(AVL* a);
 int element(AVL* a);
 int existeFG(AVL* a);
 int existeFD(AVL* a);
-int ajouterFG(AVL* a, int r);
-int ajouterFD(AVL* a, int r);
-AVL* rechercheAVL(AVL* a, int elmt);
+int  ajouterFG(AVL* a, Station s);
+int  ajouterFD(AVL* a, Station s);
+AVL* rechercheAVL(AVL* a, int ID);
 void affichePrefixeEq(AVL* a);
 void afficheInfixeEq(AVL* a);
 AVL* rotaG(AVL* a);
@@ -24,6 +25,6 @@ AVL* rotaD(AVL* a);
 AVL* doubleRotaG(AVL* a);
 AVL* doubleRotaD(AVL* a);
 AVL* equilibrage(AVL* a);
-AVL* insertionAVL(AVL* a, int r, int* h);
+AVL* insertionAVL(AVL* a, Station s, int* h);
 
 #endif 
