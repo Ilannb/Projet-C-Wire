ee68bff380fb2aa2196fea6724dbdc8b3fb28f5c
Ilannb <ilannboudria@gmail.com>
2024-12-20 12:00:57 +0100

    Update Avl.c

--- Avl.c
+++ Avl.c
@@ -1,20 +1,21 @@
  #include <stdio.h>
 #include <stdlib.h>
-#include "avl.h"
-#include "Station.h"
+#include <Avl.h>
 
-AVL* creerAVL(Station s){
-    AVL* newAVL=malloc(sizeof(newAVL));
+AVL* creerAVL(int ID,long long conso,long long capacité){
+    AVL* newAVL=malloc(sizeof(AVL));
     if(newAVL==NULL){
         exit(1);
     }
     newAVL->fg=NULL;
     newAVL->fd=NULL;
     newAVL->eq=0;
-    newAVL->val=s;
+    newAVL->conso = conso;
+    newAVL->capacite = capacite;
+    newAVL->ID = ID;
+
     return newAVL;
 }
-
 int estVide(AVL* a){
     return a==NULL;
 }
@@ -32,81 +33,8 @@ int existeFG(AVL* a){
 int existeFD(AVL* a){
     return !estVide(a) && !estVide(a->fd);
 }
-#include <stdio.h>
-#include <stdlib.h>
-#include "avl.h"
-#include "Station.h"
-
-AVL* creerAVL(Station s){
-    AVL* newAVL=malloc(sizeof(newAVL));
-    if(newAVL==NULL){
-        exit(1);
-    }
-    newAVL->fg=NULL;
-    newAVL->fd=NULL;
-    newAVL->eq=0;
-    newAVL->val=s;
-    return newAVL;
-}
-
-int estVide(AVL* a){
-    return a==NULL;
-}
-
-int estFeuille(AVL* a){
-    return estVide(a) || estVide(a->fg) && estVide(a->fd);
-}
-
-
-
-int existeFG(AVL* a){
-    return !estVide(a) && !estVide(a->fg);
-}
-
-int existeFD(AVL* a){
-    return !estVide(a) && !estVide(a->fd);
-}
-
-
-
-AVL* rechercheAVL(AVL* a, int ID){
-    if(estVide(a)){
-        return NULL;
-    }
-    else if(element(a)==ID){
-        return a;
-    }
-    else if(element(a)>ID){
-        return rechercheAVL(a->fg, ID);
-    }
-    else{
-        return rechercheAVL(a->fd, ID);
-    }
-}
-
-void affichePrefixeEq(AVL* a){
-    if(!estVide(a)){
-        printf("Valeur : %d, Equilibre : %d\n",element(a),a->eq);
-        affichePrefixeEq(a->fg);
-        affichePrefixeEq(a->fd);
-   }
-}
-
-void afficheInfixeEq(AVL* a){
-    if(!estVide(a)){
-        afficheInfixeEq(a->fg);
-        printf("Valeur : %d, equilibre : %d",a->val.ID,a->eq);
-        afficheInfixeEq(a->fd);
-   }
-}
-
-#define min(a,b) ((a) < (b) ? (a) : (b))
-#define max(a,b) ((a) > (b) ? (a) : (b))
-#define min3(a,b,c) (min(min(a,b),c))
-#define max3(a,b,c) (max(max(a,b),c))
-
-AVL* rotaG(AVL* a){
-    AVL* pivot;
+  AVL* rotaG(AVL* a){
+    AVL* pivot = a->fd;
     int eqa, eqp;
 
     a->fd = pivot->fg;
@@ -122,7 +50,7 @@ AVL* rotaG(AVL* a){
 }
 
 AVL* rotaD(AVL* a){
-    AVL* pivot;
+    AVL* pivot = a->fg;
     int eqa, eqp;
 
     a->fg = pivot->fd;
@@ -167,20 +95,22 @@ AVL* equilibrage(AVL* a){
     return a;
 }
 
-AVL* insertionAVL(AVL* a, Station s,  int* h){
+AVL* insertionAVL(AVL* a,  int* h,long long capacite,long long conso){
     if(estVide(a)){
         *h = 1;
-        return creerAVL(s);
+        return creerAVL(a->ID,capacite,conso);
     }
-    else if( s.ID < element(a)){
+    else if( a->id < element(a)){
         a->fg = insertionAVL(a->fg, s, h);
         *h = -*h;
     }
-    else if( s.ID> element(a)){
+    else if( a->id> element(a)){
         a->fd = insertionAVL(a->fd, s, h);
     }
     else{
         *h = 0;
+        a->conso += conso;
+
         return a;
     }
 
@@ -189,7 +119,6 @@ AVL* insertionAVL(AVL* a, Station s,  int* h){
         a = equilibrage(a);
         if(a->eq == 0){
             *h = 0;
-            return a;
         }
         else{
             *h = 1;
@@ -198,132 +127,53 @@ AVL* insertionAVL(AVL* a, Station s,  int* h){
     return a;
 }
 
-
-AVL* rechercheAVL(AVL* a, int ID){
-    if(estVide(a)){
-        return NULL;
+long convertionStrEntier(const char* str){
+    if(strcmp(str, "-") == 0){
+        return 0;
     }
-    else if(element(a)==ID){
-        return a;
+    return atoll(str);
+}
+
+AVL* traiterFichier(const char* NomFichier, AVL* noeud) {
+    FILE *file = fopen(NomFichier, "r");
+    if(file == NULL){
+        perror("Erreur lors de l'ouverture du fichier\n");
+        return noeud;
     }
-    else if(element(a)>ID){
-        return rechercheAVL(a->fg, ID);
-    }
-    else{
-        return rechercheAVL(a->fd, ID);
-    }
-}
 
-void affichePrefixeEq(AVL* a){
-    if(!estVide(a)){
-        printf("Valeur : %d, Equilibre : %d\n",element(a),a->eq);
-        affichePrefixeEq(a->fg);
-        affichePrefixeEq(a->fd);
-   }
-}
+    int h; 
+    int*ph = &h;
 
-void afficheInfixeEq(AVL* a){
-    if(!estVide(a)){
-        afficheInfixeEq(a->fg);
-        printf("Valeur : %d, equilibre : %d",a->val.ID,a->eq);
-        afficheInfixeEq(a->fd);
-   }
-}
+    char line[256];
 
-#define min(a,b) ((a) < (b) ? (a) : (b))
-#define max(a,b) ((a) > (b) ? (a) : (b))
-#define min3(a,b,c) (min(min(a,b),c))
-#define max3(a,b,c) (max(max(a,b),c))
+    fgets(line, sizeof(line), file);
 
-AVL* rotaG(AVL* a){
-    AVL* pivot;
-    int eqa, eqp;
+    while(fgets(line, sizeof(line), file)){
+        int ID;
+        char capaciteStr[20];
+        char consoStr[20];
+        long long capacite;
+        long long conso;
 
-    a->fd = pivot->fg;
-    pivot->fg = a;
-    eqa = a->eq;
-    eqp = pivot->eq;
-
-    a->eq = eqa - max(eqp, 0) - 1;
-    pivot->eq = max3(eqa-2, eqa+eqp-2, eqp-1);
-    a = pivot;
-
-    return a;
-}
-
-AVL* rotaD(AVL* a){
-    AVL* pivot;
-    int eqa, eqp;
-
-    a->fg = pivot->fd;
-    pivot->fd = a;
-    eqa = a->eq;
-    eqp = pivot->eq;
-
-    a->eq = eqa - max(eqp, 0) + 1;
-    pivot->eq = max3(eqa+2, eqa+eqp+2, eqp-1);
-    a = pivot;
-
-    return a;
-}
-
-AVL* doubleRotaG(AVL* a){
-    a->fd = rotaD(a);
-    return rotaG(a);
-}
-
-AVL* doubleRotaD(AVL* a){
-    a->fg = rotaG(a);
-    return rotaD(a);
-}
-
-AVL* equilibrage(AVL* a){
-    if(a->eq >= 2){
-        if(a->fd->eq >= 0){
-            return rotaG(a);
+        if (sscanf(line, "%d;%19[^;];%19s", &ID, capaciteStr, consoStr) != 3) {
+            fprintf(stderr, "Erreur de format dans la ligne : %s\n", line);
+            continue;
         }
-        else{
-            return doubleRotaG(a);
-        }
-    }
-    else if (a->eq <= -2){
-        if(a->fg->eq <= 0){
-            return rotaD(a);
-        }
-        else{
-            doubleRotaD(a);
-        }
-    }
-    return a;
-}
+        
+        capacite = convertionStrEntier(capaciteStr);
+        conso = convertionStrEntier(consoStr);
 
-AVL* insertionAVL(AVL* a, Station s,  int* h){
-    if(estVide(a)){
-        *h = 1;
-        return creerAVL(s);
-    }
-    else if( s.ID < element(a)){
-        a->fg = insertionAVL(a->fg, s, h);
-        *h = -*h;
-    }
-    else if( s.ID> element(a)){
-        a->fd = insertionAVL(a->fd, s, h);
-    }
-    else{
-        *h = 0;
-        return a;
+
+         AVL noeud =  insertionAVL(noeud,ph,capacite,conso);
+          
+ 
+        
+         
     }
 
-    if(*h != 0){
-        a->eq = a->eq + *h;
-        a = equilibrage(a);
-        if(a->eq == 0){
-            *h = 0;
-            return a;
-        }
-        else{
-            *h = 1;
-        }
-    }
-    return a;
+    fclose(file);
+    return noeud;
+
+
 }
+
